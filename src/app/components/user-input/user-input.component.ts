b91e7d44b09072e6d3ed4bc18e723a26d5d439a2
clasei <clasei@proton.me>
2023-10-10 20:06:32 +0200

    user-input udpated; dynamic textarea resizing; character count and error message added; responsive design

--- src/app/components/user-input/user-input.component.ts
+++ src/app/components/user-input/user-input.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-input.component.scss']
 })
 export class UserInputComponent {
+  userInput = '';
+  isMaxCharsReached = false;
 
+  adjustTextarea(event: Event): void {
+    let textarea: any = event.target;
+    textarea.style.overflow = 'hidden';
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+    return;
+  }
+
+  checkMaxLength(event: any): void {
+    if(event.target.value.length >= 200) {
+      this.isMaxCharsReached = true;
+    } else {
+      this.isMaxCharsReached = false;
+    }
+  }   
 }
